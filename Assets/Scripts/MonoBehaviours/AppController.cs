106c5699d0e598cf1e3398820067c0baf9c44335
Evteev Dmitriy <dev@ambidexter.io>
2017-09-01 20:26:28 +0300

    фсормированнй апдейт проекта с плагинами

---
+++ Assets/Scripts/MonoBehaviours/AppController.cs
@@ -0,0 +1,15 @@
+﻿using Assets.Scripts.Core.Services;
+using UnityEngine;
+
+// ReSharper disable once UnusedMember.Global
+// ReSharper disable once CheckNamespace
+public class AppController : MonoBehaviour
+{
+	private FacebookService _service;
+
+	// ReSharper disable once UnusedMember.Local
+	private void OnEnable()
+	{
+		_service = FacebookService.Create();
+	}
+}
